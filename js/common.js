2a43888390224c5ac871cb4d5ddab4982184edfb
namo81 <snamo81@gmail.com>
2021-09-14 15:10:31 +0900

    210914_애니메이션 관련 함수 추가

--- js/common.js
+++ js/common.js
@@ -283,3 +283,111 @@ function nBalloonSet(Ele){
 
 	if(btnClose != null) btnClose.addEventListener('click', cntHide);
 }
+
+
+/* scroll animation */
+function animateScroll(scrollObj, targetVal, duration, gap){
+	var scrollEle 	= typeof scrollObj === 'string' ? document.querySelector(scrollObj) : scrollObj,
+		gapPos 		= gap ? gap : 0,
+		dur			= duration ? duration : 500;
+	
+	var currentPos = scrollEle.scrollLeft,
+		targetPos  = targetVal - gapPos;
+	
+	animateScrollTo();
+
+	function animateScrollTo() {
+		var unit = (targetPos - currentPos) / dur;
+		var startTime = new Date().getTime();
+		var endTime = new Date().getTime() + dur;
+
+		var scrollTo = function() {
+			var now = new Date().getTime(),
+				passed = now - startTime,
+				ease = easeInOutQuad(passed / dur);
+			if (now <= endTime) {
+				scrollEle.scrollLeft = currentPos + (targetPos - currentPos) * ease;
+				requestAnimationFrame(scrollTo);
+			}
+		};
+		requestAnimationFrame(scrollTo);
+	};
+}
+
+/* ease func */
+function easeInSine(x) {
+	return 1 - Math.cos((x * Math.PI) / 2);
+}
+function easeOutSine(x) {
+	return Math.sin((x * Math.PI) / 2);
+}
+function easeInOutSine(x) {
+	return -(Math.cos(Math.PI * x) - 1) / 2;
+}
+
+function easeInQuad(x) {
+	return x * x;
+}
+function easeOutQuad(x) {
+	return 1 - (1 - x) * (1 - x);
+}
+function easeInOutQuad(x){
+	return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+}
+
+function easeInCubic(x) {
+	return x * x * x;
+}
+function easeOutCubic(x) {
+	return 1 - Math.pow(1 - x, 3);
+}
+function easeInOutCubic(x) {
+	return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+}
+
+function easeInQuart(x) {
+	return x * x * x * x;
+}
+function easeOutQuart(x) {
+	return 1 - Math.pow(1 - x, 4);
+}
+function easeInOutQuart(x) {
+	return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+}
+
+function easeInQuint(x) {
+	return x * x * x * x * x;
+}
+function easeOutQuint(x) {
+	return 1 - Math.pow(1 - x, 5);
+}
+function easeInOutQuint(x) {
+	return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+}
+
+function easeInExpo(x) {
+	return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+}
+function easeOutExpo(x) {
+	return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+}
+function easeInOutExpo(x) {
+	return x === 0
+		? 0
+		: x === 1
+		? 1
+		: x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
+		: (2 - Math.pow(2, -20 * x + 10)) / 2;
+}
+
+function easeInCirc(x) {
+	return 1 - Math.sqrt(1 - Math.pow(x, 2));
+}
+function easeOutCirc(x) {
+	return Math.sqrt(1 - Math.pow(x - 1, 2));
+}
+function easeInOutCirc(x) {
+	return x < 0.5
+	  ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
+	  : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+}
